3070c2e201d5e83ebfbca356bc8b4ca7a5c2b054
mrjeoffrey <b.jeoffrey@gmail.com>
2022-01-25 23:55:14 -0800

    started timeBlock function

--- script.js
+++ script.js
@@ -12,30 +12,42 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 
+// jQuery must-have. tells browser to use this code once DOM loaded
+$(document).ready(function() { 
 
+    // identify current day
+    $("#currentDay").text(moment().format("LLLL"));
 
-// save schedule function
-$(".saveBtn").on("click", function(){
-    var time = $(this).parent().attr("id");
-    var value = $(this).siblings(".description").val();
-    // save in local storage
-    localStorage.setItem(time, value);
-});
+    // save schedule function
+    $(".saveBtn").on("click", function(){
+        var time = $(this).parent().attr("id");
+        var value = $(this).siblings(".description").val();
+        // save in local storage
+        localStorage.setItem(time, value);
+    });
 
-// get stored schedule
-var keys = Object.keys(localStorage);
-    // loop for local storage keys
-    for (var i = 0; i < keys.length; i++) {
-        // assign storage index
-        var value = localStorage.getItem(keys[i]);
-        // add value to textarea
-        var task = $("#" + keys[i]).find("textarea");
-        task.val(value);
+    // get stored schedule
+    var keys = Object.keys(localStorage);
+        // loop for local storage keys
+        for (var i = 0; i < keys.length; i++) {
+            // assign storage index
+            var value = localStorage.getItem(keys[i]);
+            // add value to textarea
+            var task = $("#" + keys[i]).find("textarea");
+            task.val(value);
+        }
+
+    // create timeblock function loop
+    function timeBlock() {
+        // grab current hour from momentjs
+        var currentHour = moment().hours();
+            console.log(currentHour);
     }
+        // identify timeblock
+        // identify hour
+        // with current day, organize blocks into proper time frames
+    
+timeBlock();
+    
 
-// create timeblock function loop
-    // identify timeblock
-    // identify hour
-
-// identify current day
-// with current day, organize blocks into proper time frames
+});
